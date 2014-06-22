46c38e3ae860458ca0729810ea53ad94e1dad894
DC* <dc@syncr.com.ar>
2014-06-22 13:13:48 -0300

    Added few helpful git aliases

--- aliases.sh
+++ aliases.sh
@@ -27,8 +27,9 @@ alias restart_network="sudo ifconfig eth0 down && sudo ifconfig eth0 up"
 
 # git
 alias gs="git status"
+alias gC="ga . && gc -m "
 alias gd="git difftool "
-alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all | tac"
 alias ship="gp origin master"
 #alias git="hub"
 

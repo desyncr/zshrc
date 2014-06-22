88e00018a948ebf96ceb1e897c384b21cafbfd7f
DC* <dc@syncr.com.ar>
2014-06-22 13:17:05 -0300

    Added new git alias

--- aliases.sh
+++ aliases.sh
@@ -31,6 +31,7 @@ alias gC="ga . && gc -m "
 alias gd="git difftool "
 alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all | tac"
 alias ship="gp origin master"
+alias gS="gp origin master"
 #alias git="hub"
 
 # zsh related

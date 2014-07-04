3a82e8c1d5122ddb4d0938c22ef0fd95259c0afb
DC* <dc@syncr.com.ar>
2014-07-04 10:16:37 -0300

    Added work aliases

---
+++ aliases.work.sh
@@ -0,0 +1,11 @@
+alias vagrant_reboot="vagrant destroy -f && vagrant up"
+alias psqlr='/usr/local/pgsql/bin/psql -U smdev -d saludmovil'
+
+alias pingoogle="ping 8.8.8.8"
+alias restart_network="sudo ifconfig eth0 down && sudo ifconfig eth0 up"
+
+alias sm='cd /var/www/sm/development/trunk/'
+alias core='cd /home/dario/Workspace/core/trunk/'
+alias admin='cd /var/www/admin/development/trunk/'
+
+alias serverup='/home/dario/Workspace/infrastructure/serverup/serverup'

c85574dbb1fe437f5f88e3a5f84fb2c6963e49ec
DC* <dc@fansworld.tv>
2014-09-15 09:06:37 -0300

    Update aliases.work.sh

--- aliases.work.sh
+++ aliases.work.sh
@@ -9,3 +9,10 @@ alias core='cd /home/dario/Workspace/core/trunk/'
 alias admin='cd /var/www/admin/development/trunk/'
 
 alias serverup='/home/dario/Workspace/infrastructure/serverup/serverup'
+
+alias db='psql -U fw -d fw-dev-dc -h 10.30.100.42'
+export GIT_EDITOR=vim
+export VISUAL=vim
+export EDITOR=vim
+
+alias behat='bin/behat --config app/behat.yml --tags '

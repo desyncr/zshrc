b127240c89927da4d8e427b124082e89c09e1493
DC* <dario.cavuotti@gmail.com>
2015-07-09 17:19:34 -0300

    Now load alises from lib/. Fixed annoying empty lines when loading the shell

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -4,13 +4,13 @@ source $ZSH_CUSTOM/lib/functions.zsh
 # Load all environment variables
 cd $ZSH_CUSTOM
    load ".env*.zsh"
-cd -
+cd - &> /dev/null
 
 # Load antigen and bootstrap the configuration
 source $ANTIGEN/antigen.zsh
 source $ZSH_CUSTOM/.antigenrc
 
 # Finally load alises
-cd $ZSH_CUSTOM
+cd "$ZSH_CUSTOM/lib"
     load "aliases*.sh"
-cd -
+cd - &> /dev/null

6b85af57de1baaf93eebea04c74e8a1dddd87c25
DC* <dario.cavuotti@gmail.com>
2015-07-13 12:44:05 -0300

    added thefuck alias

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -25,3 +25,6 @@ if ! which pbcopy &>/dev/null && which xsel &>/dev/null; then
     alias pbcopy='xsel --clipboard --input'
     alias pbpaste='xsel --clipboard --output'
 fi
+
+# pip install thefuck
+alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'

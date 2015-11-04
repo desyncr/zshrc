6fcc7224afd3a8e922e927431d021114e7ed7101
DC* <dario.cavuotti@gmail.com>
2015-11-04 20:55:20 -0300

    added hub (hub.github.com) alias

--- lib/aliases/git.sh
+++ lib/aliases/git.sh
@@ -7,4 +7,6 @@ alias ga="git add "
 alias gf="git fetch --all"
 alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all | tac"
 
+# https://hub.github.com/
+[[ -e "/usr/local/bin/hub" ]] && alias git="hub"
 

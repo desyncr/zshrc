215430b6e1e75869f648c8a78f41e2b54f6741f5
DC* <dario.cavuotti@gmail.com>
2016-04-05 15:14:45 -0300

    Update work aliases

--- lib/aliases/aliases.work.sh
+++ lib/aliases/aliases.work.sh
@@ -1,1 +1,11 @@
-alias restart="cd ~/Workspace/boxes && ./restart && cd -"
+# restarts vagrant and runs build all
+alias restart="cd ~/Workspace/boxes && ./.restart && cd -"
+
+# updates current branch
+alias gu="git up"
+
+# updates current branch and restart vagrant (runs build all)
+alias up="gu && restart"
+
+# check outs a branch
+alias gco="git co "

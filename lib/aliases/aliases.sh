1ff90cd5cccac37128e196bbfa1025ecb20e8a5e
DC* <dario.cavuotti@gmail.com>
2016-04-05 15:16:33 -0300

    Update general aliases

--- lib/aliases/aliases.sh
+++ lib/aliases/aliases.sh
@@ -1,6 +1,7 @@
 # shortcuts
 alias mkdir="mkdir -p "
 alias ll="ls -lShra "
+alias l="ll"
 alias lt="ls -lthra "
 alias df="df -h "
 alias v="vim "
@@ -9,7 +10,7 @@ alias pwgen="< /dev/urandom tr -dc 'A-Za-z0-9\\?!=-_' | head -c13"
 alias :q="exit"
 alias reboot="sudo shutdown -r now"
 alias diskusage='du -hd1|sort -h'
-alias '#'='echo "">/dev/null'
+alias \#='echo "">/dev/null'
 
 alias pingoogle="ping 8.8.8.8"
 

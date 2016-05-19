fdf4f5577442e1d8386332c6020e104dc31ac73e
Dario <dcavuotti@afluenta.com>
2016-05-19 12:50:30 -0300

    Added git aliases for assume unchanged

--- lib/aliases/git.sh
+++ lib/aliases/git.sh
@@ -7,6 +7,13 @@ alias ga="git add "
 alias gf="git fetch --all"
 alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all | tac"
 
+# http://gitready.com/intermediate/2009/02/18/temporarily-ignoring-files.html#comment-247215295
+alias gi="git update-index --assume-unchanged "
+alias gui="git update-index --no-assume-unchanged"
+alias gid="git ls-files -v | grep ^[a-z]"
+
+alias gco="git checkout"
+
 # https://hub.github.com/
 [[ -e "/usr/local/bin/hub" ]] && alias git="hub"
 

4a40f32d2fa422a8c51da096dde5aae133ba775c
DC* <dario.cavuotti@gmail.com>
2016-08-27 21:55:10 -0300

    Update aliases

--- lib/aliases/aliases.sh
+++ lib/aliases/aliases.sh
@@ -1,17 +1,18 @@
 # shortcuts
 alias mkdir="mkdir -p "
+alias ls='ls --color'
 alias ll="ls -lShra "
 alias l="ll"
 alias lt="ls -lthra "
+
 alias df="df -h "
-alias v="vim "
-alias vi="vim -u ~/Projects/vimrc/essential.vim"
+
 alias pwgen="< /dev/urandom tr -dc 'A-Za-z0-9\\?!=-_' | head -c13"
 alias :q="exit"
 alias reboot="sudo shutdown -r now"
-alias diskusage='du -hd1|sort -h'
+alias du='du -hd1|sort -h'
 alias \#='echo "">/dev/null'
-alias '//'='echo "">/dev/null'
+alias //='echo "">/dev/null'
 
-alias pingoogle="ping 8.8.8.8"
+alias pingo="ping 8.8.8.8"
 

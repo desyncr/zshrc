5054787ff2442964922c35e20fc3da56ab8e6272
DC* <des@riseup.net>
2016-10-13 16:59:19 -0300

    Changes in bundles: added up, removed aliases

--- lib/aliases/aliases.work.sh
+++ lib/aliases/aliases.work.sh
@@ -1,11 +1,11 @@
 # restarts vagrant and runs build all
-alias restart="cd ~/Workspace/boxes && ./.restart && cd -"
+#alias restart="cd ~/Workspace/boxes && ./.restart && cd -"
 
 # updates current branch
 alias gu="git up"
 
 # updates current branch and restart vagrant (runs build all)
-alias up="gu && restart"
+#alias up="gu && restart"
 
 # check outs a branch
 alias gco="git co "

30f562922fd43abbf06e4e950153e5e51ade5719
Darío Cavuotti <des@riseup.net>
2016-12-01 15:05:12 -0300

    Update aliases for work

--- lib/aliases/aliases.work.sh
+++ lib/aliases/aliases.work.sh
@@ -10,23 +10,29 @@ alias gu="git up"
 # check outs a branch
 alias gco="git co "
 
+export AFL_ROOT="/afluenta-platform/"
+export AFL_SCRIPTS="$AFL_ROOT/afluenta_core/afluenta/scripts"
+
+alias scripts="cd $AFL_SCRIPTS"
+
 # Quickly see error logs
 alias errorlog="lnav /var/log/phperror.log /tmp/queries.afluenta.txt"
 alias err="lnav /var/log/phperror.log /tmp/queries.afluenta.txt"
+alias t="echo 'Gonna execute tests...'; sleep 5; cd $AFL_SCRIPTS && php ./test_zone.php"
 
 # Update translations
-alias translations="php ./afluenta_core/afluenta/scripts/locale_import.php master_with_dev"
+alias translations="php $AFL_SCRIPTS/locale_import.php master_with_dev"
 
 # Update actions
-alias actions="cd /afluenta-platform && php ./afluenta_web/lib/sync_actions.php && php ./afluenta_web/symfony cc && cd -"
+alias actions="cd $AFL_ROOT && php $AFL_ROOT/afluenta_web/lib/sync_actions.php && php $AFL_ROOT/afluenta_web/symfony cc && cd -"
 
 # Run workers
-alias workers="cd /afluenta-platform/afluenta_core/afluenta/scripts && php ./cmd_start_workers.php && cd -"
+alias workers="cd $AFL_SCRIPTS && php $AFL_SCRIPTS/cmd_start_workers.php"
 
 alias killmem="sudo pkill memcached"
 alias km="sudo pkill memcached"
 
-alias gruntt="cd /afluenta-platform/afluenta_web/grunt-public && grunt build-all && grunt watch"
+alias gruntt="cd $AFL_ROOT/afluenta_web/grunt-public && grunt build-all && grunt watch"
 alias gr=gruntt
 
 alias mysql="mysql --host=192.168.1.210 --user=afluenta --password=dev#root afluenta"

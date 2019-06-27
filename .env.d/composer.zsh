fc9807620e63be11af8c312bb50dd9b35a92993f
DC* <des@riseup.net>
2019-06-27 19:26:50 -0300

    Added nvm and composer to path

---
+++ .env.d/composer.zsh
@@ -0,0 +1,1 @@
+PATH="$PATH:$HOME/.composer/vendor/bin"
--- .env.d/nvm.zsh
+++ .env.d/nvm.zsh
@@ -1,4 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 PATH="$HOME/.nvm/versions/node/v8.11.3/bin/:$PATH"

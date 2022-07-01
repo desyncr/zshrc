64c4d966f63771749009014de704fa28ec83871d
Dario <dario@exads.com>
2022-07-01 23:02:00 +0100

    Use fzf with command history

--- alias/aliases.work.sh
+++ alias/aliases.work.sh
@@ -1,10 +1,14 @@
-alias exads-vagrant-tail='vagrant ssh web -c "sudo -i tail -f /var/log/php/error.log"'
-alias exads-restart-memcache='vagrant ssh web -c "sudo /bin/systemctl restart memcached.service"'
 alias phpstorm-reset='bash ~/sys-vagrant/code/phpstorm-reset.sh'
-alias my='vagrant ssh dbm -c "sudo mysql exads -pexads"'
+
 alias vp='vagrant up'
 alias down='vagrant halt'
 alias restart='vagrant halt && vagrant up'
 alias web='vagrant ssh web'
-
 alias vagrant-restart='vagrant halt && vagrant up'
+
+alias exads-tail='vagrant ssh web -c "sudo -i tail -f /var/log/php/error.log"'
+alias exads-memcache-restart='vagrant ssh web -c "sudo /bin/systemctl restart memcached.service"'
+alias exads-cc='vagrant ssh web -c "sudo -u exads -i php /var/www/jobs/campaigns-caching-v3.php --network=exads.rocks --debug"'
+alias exads-ccp='vagrant ssh web -c "sudo -u exads -i php /var/www/jobs/campaigns-caching-v3-push.php --XXX --network=exads.rocks --network-hash=e738bad9 --debug"'
+
+alias exads-my='vagrant ssh dbm -c "sudo mysql exads -pexads"'
--- alias/git.sh
+++ alias/git.sh
@@ -1,3 +1,4 @@
 alias gs="git status"
 alias gc="git commit"
 alias gd="git icdiff"
+alias up="git pull"
